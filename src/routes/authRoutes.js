511668d448752eba4f2500e2f7ce6996d97710d2
rockzsuraj <surajkmr012@gmail.com>
2021-08-19 07:24:56 +0530

    added schema

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-    console.log(req.body);
+router.post('/signup', async (req, res) => {
+    const {email, password} =  req.body;
+    try{
+    const user = new User({email, password});
+    await user.save();
+    }catch(err){
+        return res.status(422).send(err.message);
+    }
 res.send('You made a post request');
 });
 
