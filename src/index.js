9bfc9bf43eb3f108b6f10bba88d86fca6ba5d00b
rockzsuraj <surajkmr012@gmail.com>
2021-08-19 14:03:27 +0530

    added verification

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ require('./models/User.js');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes.js');
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 
 const app = express();
@@ -21,8 +22,8 @@ mongoose.connection.on('error', (err)=>{
     console.log('Error connecting to mongo', err);
     });
 
-app.get('/',(req,res)=> {
-res.send('Hi there!');
+app.get('/',requireAuth,(req,res)=> {
+res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000,()=>{
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,19 +1,22 @@
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
-
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
-    const {email, password} =  req.body;
-    try{
-    const user = new User({email, password});
-    await user.save();
-    }catch(err){
+    const { email, password } = req.body;
+    try {
+        const user = new User({ email, password });
+        await user.save();
+
+        const token = jwt.sign({ userID: user._id }, 'superSecretKey');
+        res.send({ token });
+
+    } catch (err) {
         return res.status(422).send(err.message);
     }
-res.send('You made a post request');
 });
 
 module.exports = router;
