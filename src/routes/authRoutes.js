4db165700042c8dca255be2094d7c8e4da7d57fc
rockzsuraj <surajkmr012@gmail.com>
2021-08-29 23:55:13 +0530

    added signin

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -17,6 +17,26 @@ router.post('/signup', async (req, res) => {
     } catch (err) {
         return res.status(422).send(err.message);
     }
+
+    router.post('/signin', async (req, res) => {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(422).send({error: 'Must provide email and password'});
+        }
+
+        const user = await User.findOne({email});
+         if(!user){
+             return res.status(422).send({error: 'Invalid email and password'});
+         }
+
+         try {
+            await user.comparePassword(password);
+            const token = jwt.sign({userID: user._id}, 'superSecretKey'); 
+             res.send({token});
+         } catch (err) {
+             return res.status(422).send({error: 'Invalid email and password'});
+         }
+    }); 
 });
 
 module.exports = router;
